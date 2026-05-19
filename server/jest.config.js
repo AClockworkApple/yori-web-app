b5e8de5d21cf76925831ebe7aa556a1c37810bdd
AClockworkApple <an.applee05@gmail.com>
2026-05-19 17:49:53 +0700

    Fix: Fixed CI and removed test deps

--- server/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  setupFilesAfterEnv: ['./src/test/setup.js'],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
-};
